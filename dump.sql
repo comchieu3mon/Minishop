64f24695eb6bddc08f85d8a80f5c476a1ea8b4df
DucMinhCao <minhduc.itus@gmail.com>
2020-09-21 10:01:11 +0700

    added db script

---
+++ dump.sql
@@ -0,0 +1,42 @@
+SELECT * FROM minishop.product;
+
+use minishop;
+INSERT INTO category VALUES (1, "MAN");
+INSERT INTO category VALUES (2, "MAN 1");
+INSERT INTO category VALUES (3, "MAN 2");
+INSERT INTO category VALUES (4, "MAN 3");
+INSERT INTO category VALUES (5, "MAN 4");
+
+INSERT INTO product VALUES (1, "Nike Exploration Series", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/b0cccd97-5dc7-4dba-b720-ab7863109ebf/exploration-series-basketball-t-shirt-8dkWTQ.jpg", "Men's Basketball T-Shirt", 100, 1);
+INSERT INTO product VALUES (2, "Jordan Air", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/a2bea778-e663-4e7f-9738-ba2bb0203788/jordan-air-fleece-trousers-VxtGw0.jpg", "Men's Fleece Trouser", 100, 1);
+INSERT INTO product VALUES (3, "Jordan Air", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/6e2be50d-68e8-4e2d-a6fd-771d7271bb84/jordan-air-short-sleeve-crew-k10t9C.jpg", "Men's Short-Sleeve Crew", 120, 1);
+INSERT INTO product VALUES (4, "Jordan Flight", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/144e79cd-590c-49e5-9c09-567913d120b6/jordan-flight-warm-up-jacket-tCg9QH.jpg", "Men's warn-up jackets", 150, 1);
+INSERT INTO product VALUES (5, "Nike Dri-FIT Miler", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/cb78126d-6abf-4169-826e-7453f971923e/dri-fit-miler-running-top-gJH4Zt.jpg", "Men's Running Top", 125, 1);
+
+INSERT INTO product VALUES (6, "Nike Swoosh Icon Clash", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/70c6121f-e4ec-4057-bf0a-e3788b53f197/swoosh-icon-clash-support-printed-sports-bra-M4tSLd.jpg", "Women's Medium Support", 123, 2);
+INSERT INTO product VALUES (7, "Nike Icon Clash Tiempo", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/69677db9-f9eb-4a1f-808e-e57b1aa97255/icon-clash-tempo-running-shorts-s5qnLs.jpg", "Women's Running Short", 250, 2);
+INSERT INTO product VALUES (8, "Nike Icon Clash Fast", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/25094ff4-dfea-4410-af94-c9e199bb7af5/icon-clash-fast-running-leggings-sHKJxQ.jpg", "Women's Running Leggings", 200, 2);
+INSERT INTO product VALUES (9, "Nike One Icon Clash", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/e9b3a9c6-4b55-4fa1-97e3-b41d0cfa6b91/one-icon-clash-leggings-5pW1Nv.jpg", "Women's Leggings", 300, 2);
+INSERT INTO product VALUES (10, "Nike Icon Clash 10k", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/45203076-7b49-42be-b11a-aa6391c2930a/icon-clash-10k-running-top-xn50PV.jpg", "Women's Running Top", 75, 2);
+
+INSERT INTO product VALUES (11, "Men's Running Shoes", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/nbtubci4esz9nofyrdlv/epic-phantom-react-flyknit-running-shoe-J4msqR.jpg", "Nike Epic Phantom React Flyknit", 120, 3);
+INSERT INTO product VALUES (12, "Men's Running Shoes", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/oxxrf1ohfc22hblkf1o8/legend-react-running-shoe-30157X.jpg", "Nike Legend React", 250, 3);
+INSERT INTO product VALUES (13, "Men's Running Shoes", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/e7dad55b-56d7-46ad-8558-aea05042bd0f/react-infinity-run-flyknit-running-shoe-xC1vr3.jpg", "Nike React Infinity", 1000, 3);
+INSERT INTO product VALUES (14, "Men's Running Shoes", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/1738cc7b-1e1f-4f10-bdc0-c52675b38cd0/react-infinity-run-flyknit-running-shoe-ZjGHFz.jpg", "Nike Air Force 1", 500, 3);
+INSERT INTO product VALUES (15, "Men's Running Shoes", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/9c7849e1-d4a3-488c-8479-39583a23a832/air-zoom-pegasus-37-running-shoe-vlzXhM.jpg", "Nike React Phantom Legend II", 300, 3);
+
+INSERT INTO product VALUES (16, "Men's Training Crew", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/db47e3d0-dff2-4b26-a9a5-578b3b77562c/therma-training-crew-HXLZ7p.jpg", "Nike Therma", 500, 4);
+INSERT INTO product VALUES (17, "Men's Full-Zip Training Hoodie", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/ff4bc4f0-e598-46d9-8d95-4362bf7b3e5a/therma-training-hoodie-nPRfnT.jpg", "Nike Sportwear Tech Fleece", 550, 4);
+INSERT INTO product VALUES (18, "Men's Full-Zip Hoodie", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/ff4bc4f0-e598-46d9-8d95-4362bf7b3e5a/therma-training-hoodie-nPRfnT.jpg", "Nike Therma", 760, 4);
+INSERT INTO product VALUES (19, "Men's Printed Skate Hoodie", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/6172d719-2f0d-4207-a3f4-8e617e9e5b9e/sb-printed-skate-hoodie-m1Z31T.jpg", "Nike SB", 100, 4);
+INSERT INTO product VALUES (20, "Men's Pullover Hoodie", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/25f1db3c-a74b-491b-bcb5-65c36ad04056/team-31-courtside-nba-1-2-zip-sweatshirt-cJrvJ2.jpg", "Nike Sportwear", 345, 4);
+
+INSERT INTO product VALUES (21, "Basketball Crew Socks", "https://c.static-nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/iazjwushgugqwzlo0jzx/everyday-cushioned-training-no-show-socks-R1C77V.jpg", "Lebrons Elite", 250, 5);
+INSERT INTO product VALUES (22, "Crew Socks", "https://c.static-nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/iazjwushgugqwzlo0jzx/everyday-cushioned-training-no-show-socks-R1C77V.jpg", "Nike Sportwear Essensial", 356, 5);
+INSERT INTO product VALUES (23, "Ankle Socks", "https://c.static-nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/iazjwushgugqwzlo0jzx/everyday-cushioned-training-no-show-socks-R1C77V.jpg", "Nike Racing", 50, 5);
+INSERT INTO product VALUES (24, "Ankle Socks", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/qck7shu2rmvfdajhcfvt/spark-lightweight-no-show-running-socks-XPJpmR.jpg", "Nike Spark Lightweight", 8, 5);
+INSERT INTO product VALUES (25, "Running Socks", "https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/i1-3c4545dd-4e66-47f5-ad81-477e375261b3/snkr-sox-crew-socks-QkqQ2D.jpg", "Nike Elite", 97, 5);
+
+
+select * from minishop.category;
+

8e236c2c4147a27e5775a4b4ab66081cca6b652c
DucMinhCao <minhduc.itus@gmail.com>
2020-09-29 22:19:05 +0700

    fix link on cart page

--- minishop/src/main/java/com/minhduc/controller/CartController.java
+++ minishop/src/main/java/com/minhduc/controller/CartController.java
@@ -4,16 +4,21 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.minhduc.dao.CategoryRepository;
 import com.minhduc.dto.Cart;
 
 @RequestMapping("cart/")
 @Controller
 public class CartController {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@GetMapping
 	public String getCartPage(HttpSession httpSession, ModelMap modelMap) {
@@ -21,6 +26,7 @@ public class CartController {
 			List<Cart> listCart =(List<Cart>) httpSession.getAttribute("carts");
 			modelMap.addAttribute("listCart", listCart);
 		}
+		modelMap.addAttribute("listCategory", categoryRepository.getAllCategory());
 		return "Cart";
 	}
 }
